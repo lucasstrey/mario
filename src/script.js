b6c5ab2a7632dcdbfaa6ba6ee0f5cdd3816b9e5d
Lucas Strey Benassi <lucasstreybenassi1999@hotmail.com>
2022-08-08 21:25:30 -0300

    Jogo Mario

---
+++ src/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    },500);
+}
+
+const loop = setInterval(()=>{
+
+    
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    
+    if (pipePosition <= 86 && pipePosition > 0  && marioPosition < 60) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
+
+        pipe.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+        mario.src='./images/game-over.png';
+        mario.style.width = '5.5%';
+        mario.style.marginLeft = '3%'
+         n
+        clearInterval(loop);
+    }
+
+},10)
+
+document.addEventListener('keydown', jump)
